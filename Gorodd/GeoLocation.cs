ea3fe6ba8f7bc7a8f1ef1889e42498827ce00eb6
ZemtsovSD <ZemtsovSD@206D-3>
2023-03-16 16:03:29 +0700

    Добавьте файлы проекта.

---
+++ Gorodd/GeoLocation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Gorod
+{
+    public class GeoLocation
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
+
---
+++ Gorodd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace Gorod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var city = new City();
+            city.Name = "Ekaterinburg";
+            city.Location = new GeoLocation();
+            city.Location.Latitude = 56.50;
+            city.Location.Longitude = 60.35;
+            Console.WriteLine($"I love {city.Name} located at ({city.Location.Latitude} , {city.Location.Longitude})");
+            city.Location.Longitude.ToString(CultureInfo.InvariantCulture);
+            city.Location.Latitude.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
